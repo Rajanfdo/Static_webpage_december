c612397686afd12e7d1d33d0f503dea116f42a72
Rajan-08 <udsonfdo@gmail.com>
2026-03-10 20:24:44 +0530

    added .env

--- Backend/database.py
+++ Backend/database.py
@@ -1,15 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-username = "postgres"
-password = "AcademyRootPassword"
-port = "5432"
-db_name = "full_project"
-hostname = "localhost"
+from dotenv import load_dotenv
+import os
 
-DB_URL = f"postgresql+psycopg2://{username}:{password}@{hostname}:{port}/{db_name}"
+load_dotenv()
 
-engine = create_engine(DB_URL)
+DATABASE_URL = os.getenv('DB_URL')
+
+
+
+
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
